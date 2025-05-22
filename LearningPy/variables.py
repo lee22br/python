10b7022f629094e9b1b02474338e5e0741d60c9c
lee22br <leeiglezias@gmail.com>
2025-05-22 09:02:25 -0300

    Learning Python

---
+++ LearningPy/variables.py
@@ -0,0 +1,43 @@
+# Create a variable
+greeting = "Merry Christmas"
+# Display the variable
+print(greeting)
+
+# Create the salary variable
+salary = 4950.5
+# Print the variable
+print (salary)
+
+# Assign 25 to the variable var
+var = 25
+# Print var
+print (var)
+
+# Assign "John" to the variable var
+var = "John"
+# Print var again
+print (var)
+
+# Create the favorite_food variable
+favorite_food = "steak"
+# Print the favorite_food variable
+print (favorite_food)
+# Create the food variable
+food = "pizza"
+# Assign food to favorite_food
+favorite_food = food
+# Print the favorite_food variable
+print (favorite_food)
+
+# Create the variable age
+age = 19
+# Create the message variable
+msg = "My age is:"
+# Print message and age variables
+print(msg, age)
+
+#The Great Gatsby by Fitzgerald.
+title = "The Great Gatsby"
+author = "Fitzgerald"
+# Write your code below
+print(f"{title} by {author}.")
