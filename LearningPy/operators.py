46c7f8cbeb0c95c2f1b2c39aef95181b1d91e374
lee22br <leeiglezias@gmail.com>
2025-05-22 10:37:48 -0300

    Learning Python

---
+++ LearningPy/operators.py
@@ -0,0 +1,44 @@
+total = 10 + 2
+print(total)
+total = 10 - 2
+print(total)
+total = 10 * 2
+print(total)
+total = 10 / 2
+print(total)
+total = 10 % 3
+print(total)
+total = 10 // 3
+print(total)
+total = 10 ** 3
+print(total)
+
+x = 12.5
+y = 10
+print(x + y)    # Output: 22.5
+# Create the cost_price variable
+cost_price = 25
+# Create the selling_price variable
+sell_price = 35
+# Compute the profit and print it
+profit = sell_price - cost_price
+print(profit)
+print(sell_price - cost_price)
+
+# Create pen_number and student_number variables
+pen_number = 14
+# Compute the number of pens each student will get and print it
+# Hint: find the quotient
+student_number = 3
+print("Pens for each student:", pen_number // student_number)
+# Compute and print the number of remaining pens
+# Hint: find the remainder
+print("Remaining pens:", pen_number % student_number )
+
+# Create the length variable
+length = 5
+# Calculate the volume
+vol = length ** 3
+# Print the volume
+print(vol)
+
