9a0ae488803eb9f03e64b22b7630420d07422492
leandro.iglezias <leeiglezias@gmail.com>
2025-06-08 10:18:57 -0300

    Exercism Inventory Management

--- LearningPy/inventory_management.py
+++ LearningPy/inventory_management.py
@@ -1,55 +1,52 @@
 """Functions to keep track and alter inventory."""
 
-
 def create_inventory(items):
     """Create a dict that tracks the amount (count) of each element on the `items` list.
-
     :param items: list - list of items to create an inventory from.
     :return: dict - the inventory dictionary.
     """
-
-    pass
-
+    result = dict()
+    add_items(result,items)
+    return result
 
 def add_items(inventory, items):
     """Add or increment items in inventory using elements from the items `list`.
-
     :param inventory: dict - dictionary of existing inventory.
     :param items: list - list of items to update the inventory with.
     :return: dict - the inventory updated with the new items.
     """
-
-    pass
-
+    for item in items:
+        if item not in inventory.keys():
+            inventory[item] = 0
+        inventory[item] += 1
+    return inventory
 
 def decrement_items(inventory, items):
     """Decrement items in inventory using elements from the `items` list.
-
     :param inventory: dict - inventory dictionary.
     :param items: list - list of items to decrement from the inventory.
     :return: dict - updated inventory with items decremented.
     """
-
-    pass
-
+    for item in items:
+        item_value = inventory.get(item)
+        if item_value and item_value > 0:
+            inventory[item] -= 1
+    return inventory
 
 def remove_item(inventory, item):
     """Remove item from inventory if it matches `item` string.
-
     :param inventory: dict - inventory dictionary.
     :param item: str - item to remove from the inventory.
     :return: dict - updated inventory with item removed. Current inventory if item does not match.
     """
-
-    pass
-
+    if item in inventory.keys():
+        inventory.pop(item)
+    return inventory
 
 def list_inventory(inventory):
     """Create a list containing only available (item_name, item_count > 0) pairs in inventory.
-
     :param inventory: dict - an inventory dictionary.
     :return: list of tuples - list of key, value pairs from the inventory dictionary.
     """
-
-    pass
+    return [(key, value) for (key, value) in inventory.items() if value > 0]
 
