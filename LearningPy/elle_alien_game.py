924eee0be9ef47a77ba00abd34032730fea7f75d
leandro.iglezias <leeiglezias@gmail.com>
2025-06-15 10:56:48 -0300

    Exercism Ellen s Alien Game

--- LearningPy/elle_alien_game.py
+++ LearningPy/elle_alien_game.py
@@ -1,16 +1,13 @@
 """Solution to Ellen's Alien Game exercise."""
 
-
 class Alien:
     """Create an Alien object with location x_coordinate and y_coordinate.
-
     Attributes
     ----------
     (class)total_aliens_created: int
     x_coordinate: int - Position on the x-axis.
     y_coordinate: int - Position on the y-axis.
     health: int - Number of health points.
-
     Methods
     -------
     hit(): Decrement Alien health by one point.
@@ -18,8 +15,25 @@ class Alien:
     teleport(new_x_coordinate, new_y_coordinate): Move Alien object to new coordinates.
     collision_detection(other): Implementation TBD.
     """
+    total_aliens_created = 0
+    def __init__(self, x_coordinate, y_coordinate):
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+        self.health = 3
+        Alien.total_aliens_created += 1
 
-    pass
+    def hit(self):
+        self.health -= 1
 
+    def is_alive(self):
+        return self.health > 0
 
-#TODO:  create the new_aliens_collection() function below to call your Alien class with a list of coordinates.
+    def teleport(self, new_x, new_y):
+        self.x_coordinate = new_x
+        self.y_coordinate = new_y
+
+    def collision_detection(self, other_object):
+        pass
+
+    def new_aliens_collection(positions):
+        return [Alien(position[0], position[1]) for position in positions]
